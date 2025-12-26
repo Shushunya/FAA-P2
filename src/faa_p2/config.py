5484c6773139f9baf57de728aa260e9cc7773897
Shushunya <mremarka314@gmail.com>
2025-12-26 18:46:06 +0000

    add LR

--- src/faa_p2/config.py
+++ src/faa_p2/config.py
@@ -1,10 +1,40 @@
 from pathlib import Path
 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 # Directories
 DATA_DIR = PROJECT_ROOT / "data"
+MODEL_DIR = PROJECT_ROOT / "models"
 
 # Filenames
 RAW_DATA_FILE = DATA_DIR / "BostonHousing.csv"
 CLEAN_DATA_FILE = DATA_DIR / "processed_housing.csv"
+MODEL_RESULTS_FILE = DATA_DIR / "models.csv"
+
+# ----------- MODELING ---------------
+# Model keynames
+LR, RFR, XGBR = "lr", "rfr", "xgbr"
+KEYS = [LR, RFR, XGBR]
+
+# Descriptive names mapping
+MODEL_NAMES = {
+    LR: "Linear Regression",
+    RFR: "Random Forest Regressor",
+    XGBR: "XGBRegressor",
+}
+
+MODEL_CLASSES = {LR: LinearRegression, RFR: RandomForestRegressor, XGBR: XGBRegressor}
+
+# Model config
+MODELS = {
+    key: {
+        "name": MODEL_NAMES[key],
+        "filename": MODEL_DIR / f"{key}.pkl",
+        "class": MODEL_CLASSES[key],
+    }
+    for key in KEYS
+}
